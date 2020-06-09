35410d0c355a97b290e01faf273a47055201302f
jasper <jasjamjos@gmail.com>
2020-06-09 10:46:19 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './assets/css/main.css'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="p-2 rounded bg-gray-400 m-5">Froggy Chair</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
