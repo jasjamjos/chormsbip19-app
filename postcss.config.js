4fda22b0102e13f162dfd1c5f38a12194b073515
jasper <jasjamjos@gmail.com>
2020-06-11 10:56:10 +0800

    added purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,27 @@
-const tailwindcss = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content : [
+        './src/**/*.js',
+        './src/**/*.jsx',
+        './public/index.html',
+    ],
+
+    css: ['./src/assets/css/tailwind.css'],
+
+    // Default tailwind purgecss config
+    defaultExtractor: content => {
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+    
+        return broadMatches.concat(innerMatches)
+    }
+})
 
 module.exports = {
     plugins: [
-        tailwindcss('./tailwind.config.js'),
-        require('autoprefixer')
+        require('tailwindcss'),
+        require('autoprefixer'),
+        ...process.env.NODE_ENV === 'production'
+            ? [purgecss]
+            : []
     ]
 };
